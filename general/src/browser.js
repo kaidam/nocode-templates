a675b7c076955aca15ccc2a5676ebb73ea1f4844
Kai Davenport <kaiyadavenport@gmail.com>
2019-11-22 21:06:15 +0000

    version working with dynamic paths

--- general/src/browser.js
+++ general/src/browser.js
@@ -1,6 +1,6 @@
-import App from './app'
 import Render from '@nocode-toolkit/ui/browser'
 import reducers from '@nocode-toolkit/ui/store/reducers'
+import App from './app'
 
 const render = Render({
   reducers,
