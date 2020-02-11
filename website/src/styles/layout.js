b60b4b157b6fd97fc4182baa008fff1f19971941
Kai Davenport <kaiyadavenport@gmail.com>
2020-02-11 14:47:50 +0000

    better layout for icon alignment

--- website/src/styles/layout.js
+++ website/src/styles/layout.js
@@ -25,9 +25,13 @@ const styles = makeStyles(theme => {
       boxShadow: '0px 2px 4px -1px rgba(0,0,0,0.2), 0px 4px 5px 0px rgba(0,0,0,0.14), 0px 1px 10px 0px rgba(0,0,0,0.12)',
     },
     headerToolbar: {
+      paddingLeft: '8px',
+      paddingRight: '8px',
       height: [`${theme.layout.topbarHeight}px`, '!important'],
     },
     footerToolbar: {
+      paddingLeft: '8px',
+      paddingRight: '8px',
       //height: [`${theme.layout.footerHeight}px`, '!important'],
     },
     appBarTitle: {
