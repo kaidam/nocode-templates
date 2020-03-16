9e12150a04a1d4a479cb1bc0edcc9a31998019ee
Kai Davenport <kaiyadavenport@gmail.com>
2020-03-16 17:41:11 +0000

    use generic tree component

--- website2/src/components/Tree.js
+++ website2/src/components/Tree.js
@@ -1,13 +1,10 @@
-import React, { useCallback } from 'react'
+import React, { lazy } from 'react'
 import { makeStyles } from '@material-ui/core/styles'
 import { useSelector } from 'react-redux'
 
-import systemSelectors from '@nocode-toolkit/frontend/store/selectors/system'
 import settingsSelectors from '@nocode-toolkit/frontend/store/selectors/settings'
-import useSectionTree from '@nocode-toolkit/frontend/components/hooks/useSectionTree'
-
-import List from '@material-ui/core/List'
-import TreeItem from './TreeItem'
+import SystemTree from '@nocode-toolkit/frontend/components/tree/Tree'
+const TreeItemEditor = lazy(() => import(/* webpackChunkName: "ui" */ './TreeItemEditor'))
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -20,62 +17,6 @@ const useStyles = makeStyles(theme => ({
     overflowX: 'hidden',
     flexGrow: 1,
   },
-  editor: {
-    flexGrow: 0,
-    borderBottom: '1px solid #ccc',
-  },
-  panelTop: {
-    flexGrow: 0,
-  },
-  panelBottom: {
-    flexGrow: 0,
-  },
-
-  list: {
-
-  },
-
-  itemChildren: {
-    paddingLeft: theme.spacing(1.5),
-    '& > ul': {
-      paddingTop: ['0px', '!important'],
-      paddingBottom: ['0px', '!important'],
-    }
-  },
-
-  optionsIcon: {
-    marginRight: '0px',
-    minWidth: '40px',
-  },
-  itemIcon: {
-    marginRight: '0px',
-    minWidth: '40px',
-  },
-
-  menuItem: {
-    paddingLeft: theme.spacing(0.4), 
-    paddingRight: theme.spacing(1),
-    marginLeft: theme.spacing(0.2),
-    marginTop: theme.spacing(0.2),
-    marginBottom: theme.spacing(0.2),
-    cursor: 'pointer',
-    color: theme.palette.grey[600],
-  },
-  
-  activeMenuItem: {
-    color: theme.palette.primary.main,
-    fontWeight: 'bold',
-    backgroundColor: ['#fff', '!important'],
-  },
-
-  activeColor: {
-    color: theme.palette.primary.main,
-    fontWeight: 'bold',
-  },
-
-  itemText: {
-    marginLeft: theme.spacing(1),
-  },
 }))
 
 const Tree = ({
@@ -84,51 +25,19 @@ const Tree = ({
 }) => {
   const classes = useStyles()
   const settings = useSelector(settingsSelectors.settings)
-
-  // folder pages means we treat folders as routes
-  // if this is false, then clicking
-  // on a folder just toggles it
-  // if this is true - clicking on the folder itself
-  // will open the folder route
   const folderPages = settings.folderPages === 'yes'
-
-  const onItemClick = useCallback(() => {
-    if(!folderPages) return
-    onClick()
-  }, [
-    folderPages,
-    onClick,
-  ])
-
-  const {
-    onToggleFolder,
-    list,
-  } = useSectionTree({
-    section,
-  })
 
   return (
     <div
       className={ classes.root }
     >
       <div className={ classes.content }>
-        <List
-          className={ classes.list }
-        >
-          {
-            list.map((item, i) => {
-              return (
-                <TreeItem
-                  key={ i }
-                  item={ item }
-                  folderPages={ folderPages }
-                  onToggleFolder={ onToggleFolder }
-                  onClick={ onClick }
-                />
-              )
-            })
-          }
-        </List> 
+        <SystemTree
+          section={ section }
+          folderPages={ folderPages }
+          ItemEditorComponent={ TreeItemEditor }
+          onClick={ onClick }
+        />
       </div>
     </div>
   )
--- website2/src/components/TreeItemEditor.js
+++ website2/src/components/TreeItemEditor.js
@@ -1,35 +1,20 @@
 import React, { useCallback } from 'react'
-import { makeStyles } from '@material-ui/core/styles'
 import { useDispatch } from 'react-redux'
+
+import Actions from '@nocode-toolkit/frontend/utils/actions'
+import contentActions from '@nocode-toolkit/frontend/store/modules/content'
 
 import IconButton from '@material-ui/core/IconButton'
 import MenuButton from '@nocode-toolkit/frontend/components/widgets/MenuButton'
 
-import Actions from '@nocode-toolkit/frontend/utils/actions'
-
-import contentActions from '@nocode-toolkit/frontend/store/modules/content'
-
 import icons from '@nocode-toolkit/frontend/icons'
-
 import driveUtils from '@nocode-toolkit/frontend/utils/drive'
 
 const MoreVertIcon = icons.moreVert
 
-const useStyles = makeStyles(theme => ({
-  root: {
-    display: 'flex',
-    flexDirection: 'row',
-    alignItems: 'center',
-  },
-  margin: {
-    margin: theme.spacing(1),
-  },
-}))
-
 const TreeItemEditor = ({
   item,
 }) => {
-  const classes = useStyles()
 
   const actions = Actions(useDispatch(), {
     onCreateRemoteContent: contentActions.createRemoteContent,
@@ -39,17 +24,13 @@ const TreeItemEditor = ({
   const getButton = useCallback((onClick) => {
     return (
       <IconButton
-        className={classes.margin}
         size="small"
         onClick={ onClick }
       >
         <MoreVertIcon fontSize="inherit" />
       </IconButton>
     )
-  }, [
-    classes,
-    item,
-  ])
+  }, [])
 
   const getEditorItems = useCallback(() => {
     const node = item.node
@@ -113,13 +94,11 @@ const TreeItemEditor = ({
   ])
 
   return (
-    <div className={ classes.root }>
-      <MenuButton
-        header={ item.node.name }
-        getButton={ getButton }
-        getItems={ getEditorItems }
-      />
-    </div>
+    <MenuButton
+      header={ item.node.name }
+      getButton={ getButton }
+      getItems={ getEditorItems }
+    />
   )
 }
 
