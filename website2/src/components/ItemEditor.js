6c8d43bd4f61243703980654d8e5d2c944cddb84
Kai Davenport <kaiyadavenport@gmail.com>
2020-03-30 19:58:32 +0100

    edit reset homepage

--- website2/src/components/ItemEditor.js
+++ website2/src/components/ItemEditor.js
@@ -1,14 +1,9 @@
 import React, { useCallback } from 'react'
-import { useDispatch } from 'react-redux'
-
-import Actions from '@nocode-toolkit/frontend/utils/actions'
-import contentActions from '@nocode-toolkit/frontend/store/modules/content'
-
 import IconButton from '@material-ui/core/IconButton'
 import MenuButton from '@nocode-toolkit/frontend/components/widgets/MenuButton'
 
 import icons from '@nocode-toolkit/frontend/icons'
-import driveUtils from '@nocode-toolkit/frontend/utils/drive'
+import withItemEditor from '../hooks/withItemEditor'
 
 const MoreVertIcon = icons.moreVert
 
@@ -18,15 +13,10 @@ const ItemEditor = ({
   onClick,
 }) => {
 
-  const actions = Actions(useDispatch(), {
-    onCreateRemoteContent: contentActions.createRemoteContent,
-    onEditRemoteContent: contentActions.editRemoteContent,
-    onEditLocalContent: contentActions.editLocalContent,
-    onDeleteRemoteContent: contentActions.deleteRemoteContent,
-    onDeleteLocalContent: contentActions.deleteLocalContent,
-    onHideContent: contentActions.hideContent,
-    onChangeHomepage: contentActions.changeHomepage,
-    onResetHomepage: contentActions.resetHomepage,
+  const {
+    getEditorItems,
+  } = withItemEditor({
+    node,
   })
 
   const getButton = useCallback((onClick) => {
@@ -44,125 +34,6 @@ const ItemEditor = ({
     )
   }, [
     buttonClassname,
-  ])
-
-  const getEditorItems = useCallback(() => {
-    if(node.driver == 'drive') {
-
-      const removeItem = {
-        title: 'Remove',
-        icon: icons.clear,
-        items: [{
-          title: 'Hide',
-          icon: icons.hide,
-          help: 'Hide this item but don\'t delete it from Google drive',
-          handler: () => actions.onHideContent({
-            id: node.id,
-            name: node.name,
-          }),
-        }, {
-          title: 'Delete',
-          icon: icons.delete,
-          help: 'Delete this item from Google drive',
-          handler: () => actions.onDeleteRemoteContent({
-            id: node.id,
-            driver: node.driver,
-            name: node.name,
-          }),
-        }]
-      }
-
-      const openUrl = driveUtils.getItemUrl(node)
-      if(node.type == 'folder') {
-        return [{
-          title: 'Add',
-          icon: icons.add,
-          items: [{
-            title: 'Folder',
-            icon: icons.folder,
-            secondaryIcon: icons.drive,
-            handler: () => actions.onCreateRemoteContent({
-              title: 'Create Folder',
-              driver: 'drive',
-              form: 'drive.folder',
-              parentId: node.id,
-            })
-          },{
-            title: 'Document',
-            icon: icons.docs,
-            secondaryIcon: icons.drive,
-            handler: () => actions.onCreateRemoteContent({
-              title: 'Create Document',
-              driver: 'drive',
-              form: 'drive.document',
-              parentId: node.id,
-            })
-          }],
-        }, {
-          title: 'Edit',
-          icon: icons.edit,
-          handler: () => actions.onEditRemoteContent({
-            title: `Edit ${node.type.replace(/^\w/, st => st.toUpperCase())}`,
-            driver: 'drive',
-            form: `drive.${node.type}`,
-            id: node.id,
-          })
-        }, {
-          title: 'Open in Drive',
-          icon: icons.open,
-          secondaryIcon: icons.drive,
-          url: openUrl,
-        }, 
-        removeItem,
-        ]
-      }
-      else {
-        return [
-          {
-            title: 'Edit',
-            icon: icons.edit,
-            secondaryIcon: icons.drive,
-            url: openUrl,
-          }, 
-          node.isHome ? 
-            {
-              title: 'Change Homepage',
-              icon: icons.search,
-              secondaryIcon: icons.drive,
-              handler: actions.onChangeHomepage,
-            } : removeItem,
-          node.defaultDocumentId != node.id ? 
-            {
-              title: 'Reset Homepage',
-              icon: icons.reset,
-              secondaryIcon: icons.drive,
-              handler: actions.onResetHomepage,
-            } : null
-        ].filter(i => i)
-      }
-    }
-    else {
-      return [{
-        title: 'Edit',
-        icon: icons.edit,
-        handler: () => actions.onEditLocalContent({
-          title: `Edit ${node.type.replace(/^\w/, st => st.toUpperCase())}`,
-          driver: node.driver,
-          form: node.type,
-          id: node.id,
-          location: node.location,
-        })
-      }, {
-        title: 'Delete',
-        icon: icons.delete,
-        handler: () => actions.onDeleteLocalContent({
-          id: node.id,
-          name: node.name,
-        }),
-      }]
-    }
-  }, [
-    node,
   ])
 
   return (
