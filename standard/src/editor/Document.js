05d397915e80f5b3027ebe57cc1bcf290a3a52e8
Kai Davenport <kaiyadavenport@gmail.com>
2020-04-12 18:26:41 +0100

    styles for edit buttons

--- standard/src/editor/Document.js
+++ standard/src/editor/Document.js
@@ -20,18 +20,32 @@ const useStyles = makeStyles(theme => ({
   root: {
     display: 'flex',
     flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'flex-start',
     borderRadius: '24px',
     padding: theme.spacing(0.75),
     backgroundColor: theme.palette.grey[200],
   },
-  icon: {
-    marginRight: theme.spacing(0.5),
-    padding: theme.spacing(0.5),
-    borderRadius: '24px',
+  iconSection: {
+    flexGrow: 1,
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  leftIcons: {
+    justifyContent: 'flex-start',
+  },
+  rightIcons: {
+    justifyContent: 'flex-end',
+  },
+  iconContainer: {
+    marginLeft: theme.spacing(0.25),
+    marginRight: theme.spacing(0.25),
+    padding: theme.spacing(0.2),
+    borderRadius: '16px',
     backgroundColor: '#fff',
-  }
+  },
+  icon: {
+    fontSize: '0.85em',
+  },
 }))
 
 const DocumentEditor = ({
@@ -62,6 +76,7 @@ const DocumentEditor = ({
           <AddIcon
             fontSize="inherit"
             color="secondary"
+            className={ classes.icon }
           />
         </IconButton>
       </Tooltip> 
@@ -72,70 +87,78 @@ const DocumentEditor = ({
 
   return (
     <div className={ rootClassname }>
-      <div className={ classes.icon }>
-        <Tooltip title="Settings" placement="top">
-          <IconButton
-            size="small"
-            onClick={ onOpenSettings }
-          >
-            <SettingsIcon
-              fontSize="inherit"
-              color="primary"
-            />
-          </IconButton>
-        </Tooltip>
-      </div>
-      {
-        driveUtils.isFolder(node) && (
-          <div className={ classes.icon }>
-            <Tooltip title="Edit" placement="top">
-              <IconButton
-                size="small"
-                onClick={ onEditItem }
-              >
-                <EditIcon
-                  fontSize="inherit"
-                  color="primary"
-                />
-              </IconButton>
-            </Tooltip>
-          </div>
-        )
-      }
-      <div className={ classes.icon }>
-        <Tooltip
-          title={
-            driveUtils.isFolder(node) ?
-              "Open Google Folder" :
-              "Edit Google Document"
-          }
-          placement="top">
-          <IconButton
-            size="small"
-            onClick={ onOpenItem }
-          >
-            {
-              driveUtils.isFolder(node) ? (
-                <OpenIcon
-                  fontSize="inherit"
-                  color="primary"
-                />
-              ) : (
-                <EditIcon
-                  fontSize="inherit"
-                  color="primary"
-                />
-              )
+      <div className={ classnames(classes.iconSection, classes.leftIcons) }>
+        <div className={ classes.iconContainer }>
+          <Tooltip title="Settings" placement="top">
+            <IconButton
+              size="small"
+              onClick={ onOpenSettings }
+            >
+              <SettingsIcon
+                fontSize="inherit"
+                color="primary"
+                className={ classes.icon }
+              />
+            </IconButton>
+          </Tooltip>
+        </div>
+        {
+          driveUtils.isFolder(node) && (
+            <div className={ classes.iconContainer }>
+              <Tooltip title="Edit" placement="top">
+                <IconButton
+                  size="small"
+                  onClick={ onEditItem }
+                >
+                  <EditIcon
+                    fontSize="inherit"
+                    color="primary"
+                    className={ classes.icon }
+                  />
+                </IconButton>
+              </Tooltip>
+            </div>
+          )
+        }
+        <div className={ classes.iconContainer }>
+          <Tooltip
+            title={
+              driveUtils.isFolder(node) ?
+                "Open Google Folder" :
+                "Edit Google Document"
             }
-          </IconButton>
-        </Tooltip>
+            placement="top">
+            <IconButton
+              size="small"
+              onClick={ onOpenItem }
+            >
+              {
+                driveUtils.isFolder(node) ? (
+                  <OpenIcon
+                    fontSize="inherit"
+                    color="primary"
+                    className={ classes.icon }
+                  />
+                ) : (
+                  <EditIcon
+                    fontSize="inherit"
+                    color="primary"
+                    className={ classes.icon }
+                  />
+                )
+              }
+            </IconButton>
+          </Tooltip>
+        </div>
       </div>
-      <div className={ classes.icon }>
-        <MenuButton
-          noHeader
-          getButton={ getAddButton }
-          getItems={ getAddItems }
-        />
+      <div className={ classnames(classes.iconSection, classes.rightIcons) }>
+        <div className={ classes.iconContainer }>
+          <MenuButton
+            noHeader
+            getButton={ getAddButton }
+            getItems={ getAddItems }
+          />
+        </div>
       </div>
     </div>
   )
