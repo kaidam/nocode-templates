cc7a92bc3f9ceb130239faf574991f54e53e15cc
Kai Davenport <kaiyadavenport@gmail.com>
2020-07-21 11:17:06 +0100

    inject initial state at publish time

--- standard/src/pages/Layout.js
+++ standard/src/pages/Layout.js
@@ -31,7 +31,7 @@ const Layout = ({
   children,
 }) => {
   const showUI = useSelector(systemSelectors.showUI)
-  
+
   const contentRef = useRef(null)
   
   const settings = useSelector(settingsSelectors.settings)
