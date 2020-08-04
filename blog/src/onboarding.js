84198603323968647e22d28c0b0ac30f4362a8cb
Kai Davenport <kaiyadavenport@gmail.com>
2020-08-04 18:37:23 +0100

    blog onboarding

--- blog/src/onboarding.js
+++ blog/src/onboarding.js
@@ -14,11 +14,74 @@ const ONBOARDING_STEPS = {
     description: FolderInfoDescription,
     smallDescription: FolderInfoDescription,
   }),
+  addSectionContent: () => ({
+    id: 'navbar_blogposts_full',
+    element: 'navbar_blogposts_full',
+    type: 'focus',
+    title: 'Creating Content',
+    //offset: '0, 20',
+    disableClick: true,
+    description: [
+      'Each blog post on your website is loaded from a Google document.',
+      'Create new blog posts by clicking the add button.',
+      'Make sure you create "tags" for your posts, this will create menu items for each tag'
+    ],
+    smallDescription: [
+      'Each blog post on your website is loaded from a Google document.',
+      'Create new blog posts by clicking the add button.',
+      'Make sure you create "tags" for your posts, this will create menu items for each tag'
+    ],
+  }),
+  editSettings: () => ({
+    id: 'editSettings',
+    element: 'editSettings',
+    type: 'focus',
+    title: 'Editing Settings',
+    offset: '0, 20',
+    disableClick: true,
+    initialise: async (dispatch, getState) => {
+      await dispatch(uiActions.setSettingsOpen(true))
+      await Promise.delay(300)
+    },
+    description: [
+      'You can configure many things for your website using the settings button.',
+      'Change layout settings, activate plugins, add custom domains, configure security and many more settings can configured here.',
+    ],
+    smallDescription: [
+      'You can configure many things for your website using the settings button.',
+      'Change layout settings, activate plugins, add custom domains, configure security and many more settings can configured here.',
+    ],
+  }),
+  publishWebsite: () => ({
+    id: 'publishWebsite',
+    element: 'publishWebsite',
+    type: 'focus',
+    title: 'Publishing Your Website',
+    offset: '0, 20',
+    disableClick: true,
+    cleanup: async (dispatch, getState) => {
+      await Promise.delay(300)
+      await dispatch(uiActions.setSettingsOpen(false))
+    },
+    description: [
+      'So people can see your amazing content, we need to "publish" it.',
+      'This will convert all of your Google documents and combine them into a website.',
+      'It will then give you a URL that you can send to your vistors.',
+    ],
+    smallDescription: [
+      'So people can see your amazing content, we need to "publish" it.',
+      'This will convert all of your Google documents and combine them into a website.',
+      'It will then give you a URL that you can send to your vistors.',
+    ],
+  }),
 }
 
 const ONBOARDING = {
   steps: [
     ONBOARDING_STEPS.sectionFolders(),
+    ONBOARDING_STEPS.addSectionContent(),
+    ONBOARDING_STEPS.editSettings(),
+    ONBOARDING_STEPS.publishWebsite(),
   ]
 }
 
